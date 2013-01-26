89e2a26cabb4a1ed012f9882cf74d4c9800b241b
numidiasoft <tarik.ihadjadene@gmail.com>
2013-01-26 14:41:53 +0100

    add log4r logger

--- lib/simple-delicious/logger.rb
+++ lib/simple-delicious/logger.rb
@@ -2,9 +2,14 @@ require "log4r"
 
 module Simple
   module Delicious
-    class Logger
-      class << self
-      end
+    module Log
+        include Log4r
+
+        def self.new(name, outputer = Outputter.stdout )
+          @logger = Logger.new name
+          @logger.outputters = outputer and @logger
+        end #end of initialize(name)
+
     end
   end
 end
